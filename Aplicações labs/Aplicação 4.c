07d054225d5c21c698090a9bfe80ae3480d99155
Luiza <43518042+lcortezs@users.noreply.github.com>
2020-09-03 18:27:02 -0300

    Update Aplicação 4.c

--- Aplicações labs/Aplicação 4.c
+++ Aplicações labs/Aplicação 4.c
@@ -5,30 +5,29 @@
 * Author : Ana Watanabe
 */
 
-#define F_CPU 16000000UL /*define a frequência do microcontrolador 16MHz
-(necessário para usar as rotinas de atraso)*/
+#define F_CPU 16000000UL 
 #include <avr/io.h> //definições do componente especificado
-//#include <C:\Users\Ana Watanabe\Documents\Atmel
-Studio\7.0\GccApplication2\GccApplication2\Header\gpio.h>
 #include <util/delay.h> //para incluir rotina _delay_ms()
 
 int main(void)
 {
 unsigned char i;
+
 // led saida PB3
 DDRB = 0b00001000; // configura o bit 3 => PB3
- // buzzer saida PD3
+ 
+// buzzer saida PD3
 DDRD = 0b00001000; // configura o bit 3 => PD3
- // saida PB3 para OC2A página 203
+ 
+// saida PB3 para OC2A página 203
 // saida PD3 para OC2B
 TCCR2A = 0b11100001; //MODO 1 para PWM fase corrigida, saida
 OC2A(INVERTIDA) e OC2B
 // NÃO invertida TOP = 0xFF, OC2A e OC2B habilitados
 TCCR2B = 0b00000001; // liga TC2, prescaler = 1
-OCR2B = 0; //controle do ciclo ativo do PWM (0%) no pino OC2B => desligar
-o buzzer
- OCR2A = 0; //controle do ciclo ativo do PWM (0%) no pino OC2A => desligar o
-led
+OCR2B = 0; //controle do ciclo ativo do PWM (0%) no pino OC2B => desligar o buzzer
+OCR2A = 0; //controle do ciclo ativo do PWM (0%) no pino OC2A => desligar o led
+ 
  while (1)
 {
 for(i = 255; i > 0; i =i-5) // inicia com 100% - led 100% aceso
